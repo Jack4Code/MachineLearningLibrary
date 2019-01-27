29db48b517aa4225a599f89df6b15a9abd2e5086
jack.giannini@gmail.com <jack.giannini@gmail.com>
2019-01-26 21:15:52 -0500

    Add project files.

---
+++ MachineLearningLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearningLibrary
+{
+    public class Class1
+    {
+    }
+}
